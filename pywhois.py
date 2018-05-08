50ea7f6f43af382689756b6da25eb4ded715d138
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-05-08 23:29:51 +0430

    test

--- pywhois.py
+++ pywhois.py
@@ -4,10 +4,11 @@ import whois
 domain = input("Please enter your domain: ")
 matchObj = \
     re.match(r'(?:http|https):\/\/((?:[\w-]+)(?:\.[\w-]+)+)(?:[\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?',
-             domain,  re.I)
+             domain, re.I)
 if matchObj:
     w = whois.whois(domain)
     print(w)
 else:
     print("Your domain is not valid!")
 
+'''look'''
