8e17f6292427075209abff25781bc6a3c868ace3
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-06-08 13:40:10 +0430

    Some changes

--- pywhois.py
+++ pywhois.py
@@ -1,14 +1,37 @@
+import getopt
 import re
 import whois
+import sys
 
-domain = input("Please enter your domain: ")
+domain = ''
+argv = sys.argv[1:]
+
+try:
+    opts, args = getopt.getopt(argv, "d:", ["domain="])
+except getopt.GetoptError:
+    print('python pywhois.py -d <domain> ')
+    print('python pywhois.py --domain <domain> ')
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ("-d", "--domain"):
+        domain = arg
+
+if domain == '':
+    domain = input("Please enter your domain: ")
+
 matchObj = \
     re.match(r'(?:http|https):\/\/((?:[\w-]+)(?:\.[\w-]+)+)(?:[\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?',
              domain, re.I)
 if matchObj:
     w = whois.whois(domain)
-    print(w)
+    whereOut = input("Save to file or display on monitor? f/m ")
+    if whereOut.lower() == "f" or whereOut.lower() == "file":
+        filename = matchObj.group(1).replace(".", "") + ".txt"
+        myfile = open(filename, 'w')
+        myfile.write(w.text)
+        myfile.close()
+    else:
+        print(w)
 else:
     print("Your domain is not valid!")
-
-'''look'''
